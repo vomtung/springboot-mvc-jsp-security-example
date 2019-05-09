a92442d61e645822baf828aff6aef8a64bb010c1
Tung Vo <vominhtung@tma.com.vn>
2019-05-09 14:33:34 +0700

    Greeting example

--- springboot-mvc-jsp-security-example/src/main/java/com/example/demo/controller/GreetingController.java
+++ springboot-mvc-jsp-security-example/src/main/java/com/example/demo/controller/GreetingController.java
@@ -2,17 +2,29 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Greeting;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model)
+	@GetMapping("/greeting")
+	public String greeting(Model model)
+	{
+	     model.addAttribute("greeting", new Greeting());
+	       return "greeting";
+	}
+    
+    @PostMapping("/greeting")
+    public String postGreeting(@ModelAttribute("greeting") Greeting greeting, ModelMap model)
     {
-        model.addAttribute("name", name);
-        return "greeting";
+    	model.addAttribute("name", greeting.getContent());
+    
+        return "resultGreeting";
     }
 
 }
--- springboot-mvc-jsp-security-example/src/main/webapp/WEB-INF/jsp/greeting.jsp
+++ springboot-mvc-jsp-security-example/src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -1,10 +1,9 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Greeting</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-<body>
-	Greeting
-</body>
-</html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
+<spring:url value="/greeting" var="userActionUrl" />
+<p>Enter your name</p>
+<form:form method="post" modelAttribute="greeting" action="${userActionUrl}">
+	<form:input path="content" type="text" /> <!-- bind to user.name-->
+	<input type="submit" value="Submit"/>
+</form:form>
